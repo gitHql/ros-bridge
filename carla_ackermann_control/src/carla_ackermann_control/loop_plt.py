66c6d410d3095d27c0943c53904fddbfed921034
heqinglong <qinglong.he@newrizon.com>
2022-12-13 19:01:29 +0800

    绘图:0轴采用虚线,以免实际加速度为0的时候相互看不见

--- carla_ackermann_control/src/carla_ackermann_control/loop_plt.py
+++ carla_ackermann_control/src/carla_ackermann_control/loop_plt.py
@@ -7,36 +7,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def clean_length(imu_hz):
-    return 20 * imu_hz
+    return 200 * imu_hz
 
 
-def plot_pid_imureal(pedal_history, pid_val, real_val, output_throttles):
+def plot_pid_imureal(pedal_history, pid_val, real_val, output_throttles, brake_upper_borders):
     plt.ion()  
 
-    high =  [val + (0.1 if val > 0 else 0.2) for val in pid_val]
-    a , = plt.plot(high, label='pid_up')
-
     a , = plt.plot(pid_val, label='pid want acc')
+
+    b, = plt.plot(real_val,  linewidth=2, label='real_acc')
 
     low = [val - (0.1 if val > 0 else 0.2 )for val in pid_val]
     a , = plt.plot(low, label='pid_low')
 
-    b, = plt.plot(real_val, label='real_acc')
+    high =  [val + (0.1 if val > 0 else 0.2) for val in pid_val]
+    a , = plt.plot(high, label='pid_up')
+
     c, = plt.plot(pedal_history, label = 'target pedal')
     d, = plt.plot(output_throttles, label = 'output_throttles')
+
+    d, = plt.plot(brake_upper_borders, label = 'brake_upper_borders')
+    
+
+    zero =  [0 for val in pid_val]
+    a , = plt.plot(zero,  linestyle='dotted', marker='', label='zero')
 
     leg = plt.legend(loc='upper right', prop={'size': 6})
     # leg = plt.legend(loc='lower right', prop={'size': 6})
 
-
-    # length = len(pid_val)
-    # my_x_ticks = np.arange(0, length, 1 if length < 100 else 5)
-    # my_y_ticks = np.arange(-2, 5, 0.1)
-    # plt.xticks(my_x_ticks)
-    # plt.yticks(my_y_ticks)
-
     plt.show()
     plt.pause(0.01)
     plt.clf()  #清除图像
-
-count = 0
